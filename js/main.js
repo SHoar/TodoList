de9f6dbbaff0da2c9e666dc31ae9dea44bb18a42
Sean Hoar <sean.hoar@gmail.com>
2017-04-24 08:22:40 -0400

    Add confirm message for DELETE

--- js/main.js
+++ js/main.js
@@ -1,17 +1,18 @@
 $(document).ready(() => {
   $('.delete-todo').on('click', (e) => {
-    $target = $(e.target);
-    const id = $target.attr('data-id');
-    $.ajax({
-      type:'DELETE',
-      url: '/todo/delete/'+id,
-      success: (response) => {
-        confirm("Delete Todo?");
-        window.location.href= '/';
-      },
-      error: (error) => {
-        console.log(error);
-      }
-    });
+    if (confirm("Delete Todo?")){
+      $target = $(e.target);
+      const id = $target.attr('data-id');
+      $.ajax({
+        type:'DELETE',
+        url: '/todo/delete/'+id,
+        success: (response) => {
+          window.location.href= '/';
+        },
+        error: (error) => {
+          console.log(error);
+        }
+      });
+    }
   });
 });
